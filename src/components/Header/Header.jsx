730dc1ba1125157d77de715a5bbb7535be8c9798
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-03-04 15:14:36 +0100

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
+import { Link } from "react-router-dom";
 import { HeroSlider } from "../Slider/Slider.jsx";
+import Styles from "./Header.module.scss"
 
 export const Header = () => {
     return (
-        <>
-            <div>
+        <header>
+            <div className={Styles.hero}>
              <HeroSlider />
+            
+            <div className={Styles.container}>
+                <h2>Find og anmeld genbrugsstationer</h2>
+                <div className={Styles.buttonContainer}>
+                    <Link>Find station</Link>
+                    <Link>Log ind</Link>
+                </div>
             </div>
-        </>
+            </div>
+        </header>
+
     )
 }
